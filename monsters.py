604f3b66f2995e71315778d40b9a428d0c646527
Ryan Jepson <RyanJepson.webdev@gmail.com>
2022-12-13 14:22:01 -0500

    Inital load

---
+++ monsters.py
@@ -0,0 +1,27 @@
+"""Program for Aariel
+    gives you a random name of a monster hunter monster
+"""
+
+from random import randint
+def main():
+    print("Generates a random monster name from Monster Hunter Series")
+    monster_names = []
+    with open("Monsters.txt", 'r') as txt:
+        total = 0
+        for line in txt:
+            monster = line
+            monster = monster.strip()
+            monster_names.append(monster)
+            total += 1
+    while True:
+        index = randint(0, total)
+        print(monster_names[index])
+        more = input('Get another monster name? (Y/N): ')
+        more = more.upper()
+        if more == 'N':
+            exit()
+        else:
+            continue
+
+if __name__ == "__main__":
+    main()
